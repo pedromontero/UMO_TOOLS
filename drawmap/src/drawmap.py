72de75d46b41d6873cd835c840e23acf7641966e
Pedro Montero <pmontero@intecmar.gal>
2021-10-13 12:17:54 +0200

    boundary_box object is included for map limits, reader is able to deal with NetCDF and HDF files depending on file extension drawcurrents uses coordinates rank to mapping and boundary box

--- drawmap/src/drawmap.py
+++ drawmap/src/drawmap.py
@@ -21,6 +21,8 @@ import numpy as np
 from common import read_input
 from common.readers import reader_HDF
 from common.readers import reader_NetCDF
+from common.boundarybox import BoundaryBox
+
 from drawcurrents import drawcurrents
 
 
@@ -40,7 +42,7 @@ def main():
 
 
     # Read input file
-    input_file = 'drawmap.json'
+    input_file = 'drawmap_ncdf.json'
     input_keys = ['path_in',
                   'file_in',
                   'file_out',
@@ -69,13 +71,25 @@ def main():
     file_name = os.path.join(file_path, file_hdf)
     style = inputs['style']
     limits = inputs['limits']
+    boundary_box =BoundaryBox(limits[0], limits[1], limits[2], limits[3])
 
     u_name = inputs['u']
     v_name = inputs['v']
 
     print('Opening: {0}'.format(file_name))
 
-    reader = reader_HDF.ReaderHDF(file_name)
+    hdf = False
+    ncdf = False
+
+    extension = file_hdf.split('.')[1]
+    hdf = (extension == 'hdf' or extension == 'hdf5')
+    ncdf = (extension == 'nc' or extension == 'nc4')
+    print(hdf, ncdf)
+
+    if hdf:
+        reader = reader_HDF.ReaderHDF(file_name)
+    elif ncdf:
+        reader = reader_NetCDF.ReaderNetCDF(file_name)
 
     data = reader.get_date(time)
     data_str = data.strftime("%Y-%m-%d %H:%M UTC")
@@ -92,19 +106,26 @@ def main():
     nlon = reader.n_longitudes
     nlat = reader.n_latitudes
 
-    lats = lat[0:nlat - 1]
-    lons = lon[0:nlon - 1]
+    if reader.coordinates_rank == 1:
+        lats = lat[0:nlat - 1]
+        lons = lon[0:nlon - 1]
+    elif reader.coordinates_rank == 2:
+        lats = lat[0:nlon-1, 0:nlat - 1]
+        lons = lon[0:nlon-1, 0:nlat - 1]
 
-    us = u[level, 0:nlon - 1, 0:nlat - 1]
-    vs = v[level, 0:nlon - 1, 0:nlat - 1]
-    ust = np.transpose(us)
-    vst = np.transpose(vs)
+    if len(u.shape) == 3:
+        print(f'entro y nlon = {nlon} y nlat = {nlat}')
+        us = u[level, 0:nlat - 1, 0:nlon - 1]
+        vs = v[level, 0:nlat - 1, 0:nlon - 1]
+    elif len(u.shape) == 2:
+        us = u[0:nlat - 1, 0:nlon - 1]
+        vs = v[0:nlat - 1, 0:nlon - 1]
 
-    mod = pow((pow(ust, 2) + pow(vst, 2)), .5)
+    mod = pow((pow(us, 2) + pow(vs, 2)), .5)
 
     reader.close()
 
-    drawcurrents(nx, ny, scale, resolution, level, time, lats, lons, ust, vst, mod, file_out, title, style, limits)
+    drawcurrents(reader.coordinates_rank, nx, ny, scale, resolution, level, time, lats, lons, us, vs, mod, file_out, title, style, boundary_box)
 
 
 if __name__ == '__main__':
