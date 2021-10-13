2658e07128ce824f6d61745f4d5453e7ede69a15
Pedro Montero <pmontero@intecmar.gal>
2021-10-13 12:12:17 +0200

    class Reader with get_rank and commented

--- common/readers/reader.py
+++ common/readers/reader.py
@@ -1,13 +1,35 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+**reader.py**
+
+* *Purpose:* Abstract class of reader object. Reader object deals with reading HDF, NetCDF son classes
+
+* *python version:* 3.9
+* *author:* Pedro Montero
+* *license:* INTECMAR
+* *requires:*
+* *date:* 2021/10/13
+* *version:* 1.0.0
+* *date version* 2021/10/13
+
+
+"""
+
 from abc import ABC, abstractmethod
 
 
 class Reader(ABC):
+    """ Abstract class of Reader object"""
     def __init__(self, file):
+        """ Open a file and get longitudes and latitudes and coordinates rank"""
         self.dataset = self.open(file)
         self.n_longitudes = None
         self.n_latitudes = None
         self.longitudes = self.get_longitudes()
         self.latitudes = self.get_latitudes()
+        self.coordinates_rank = self.get_rank(self.longitudes)
 
     @abstractmethod
     def open(self, file):
@@ -32,4 +54,10 @@ class Reader(ABC):
     @abstractmethod
     def get_date(self, n_time):
         pass
+
+    @staticmethod
+    def get_rank(array) -> int:
+        """ Number of dimensions of an array"""
+        return len(array.shape)
+
 
