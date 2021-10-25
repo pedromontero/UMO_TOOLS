65f54efe08e15364c0bb37ddb9f95a366cfc946e
Pedro Montero <pmontero@intecmar.gal>
2021-10-25 12:31:24 +0200

    download finished

--- download_models/download_hydro.py
+++ download_models/download_hydro.py
@@ -22,13 +22,7 @@ yyyymmdd INICIAIS MUDANZA
 import os
 import datetime
 import urllib.request
-
-class PathIn:
-
-    file_type = {'hydro', 'hydro_hist'}
-    prefix: str
-    sufix: str
-
+from collections import namedtuple
 
 
 def main():
@@ -42,49 +36,68 @@ def main():
 
     """
     # input
-    hydro_log = True
-
-    hist_hydro_log = True
-    hist_met_log = False
-    date_ini = datetime.date(2020, 2, 20)
-    days = 1
+    type_url = 'hydro_hist'
+    name_grid = 'AROUSA'
+    date_ini = datetime.date(2017, 3, 30)
+    days = 0
     # end input
 
     date_fin = date_ini + datetime.timedelta(days=days)
     dates = [date_ini + datetime.timedelta(days=d) for d in range((date_fin - date_ini).days + 1)]
 
-
-    prefix_hydro = 'MOHID_Hydrodynamic_Arousa_'
-    sufix_hydro = '_0000.hdf5'
-
-    if hist_hydro_log:
-        prefix_url_hydro = r'http://mandeo.meteogalicia.es/thredds/fileServer/modelos/mohid/history/arousa/'
-    else:
-        prefix_url_hydro = r'http://mandeo.meteogalicia.es/thredds/fileServer/mohid_arousa/fmrc/files/'
-
-
     os.chdir(r'.\..')
     root = os.getcwd()
     path_in = os.path.join(root, 'datos/download_models')
 
-    # Download hydro
-    if hydro_log:
-        for n, date in enumerate(dates):
-            file_in = prefix_hydro + date.strftime('%Y%m%d') + sufix_hydro
-            ffile_in = os.path.join(path_in, file_in)
-            print(ffile_in)
-            if os.path.exists(ffile_in):
-                print('xa atopei o ficheiro', ffile_in)
-            else:
-                if hist_hydro_log:
-                    url = prefix_url_hydro + file_in
-                else:
-                    url = prefix_url_hydro + date.strftime('%Y%m%d') + '/' + file_in
-                print('Voy baixar {0} a {1}'.format(url, ffile_in))
-                urllib.request.urlretrieve(url, ffile_in)
+    for n, date in enumerate(dates):
+
+        url = Url()
+        ffile_in = os.path.join(path_in, url.get_file(type_url, name_grid, date))
+        print(ffile_in)
+        if os.path.exists(ffile_in):
+            print('xa atopei o ficheiro', ffile_in)
+        else:
+            url_grid = url.get_url(type_url, name_grid, date)
+            print('Voy baixar {0} a {1}'.format(url_grid, ffile_in))
+            urllib.request.urlretrieve(url_grid, ffile_in)
 
     print('End')
 
 
+class Url:
+
+    """Class for building URL for models"""
+
+    def __init__(self):
+        Grid = namedtuple('Grid', ['grid', 'capital_grid', 'grid_mohid'])
+        self.GRID = {'ARTABRO': Grid('artabro', 'Artabro', 'artabro'),
+                     'AROUSA': Grid('arousa', 'Arousa', 'arousa'),
+                     'NOIA': Grid('noia_muros', 'NoiaMuros', 'noiamuros'),
+                     'PORTOCORUNA': Grid('porto_coruna', 'CorunaP', 'portocoruna'),
+                     'PORTOLANGOSTEIRA': Grid('porto_Langosteira', 'LangosteiraP', 'portolangosteira'),
+                     'VIGO': Grid('vigo_pontevedra', 'Vigo', 'vigo')}
+
+    def get_url(self, type_url,name_grid, date):
+
+        grid = self.GRID[name_grid]
+        URL = {'hydro_raw':
+           f'http://mandeo.meteogalicia.es/thredds/fileServer/modelos/mohid/rawoutput/{grid.grid}/%Y%m%d/',
+           'hydro_hist':
+           f'http://mandeo.meteogalicia.es/thredds/fileServer/modelos/mohid/history/{grid.grid}/',
+           'hydro_nc':
+           f'http://mandeo.meteogalicia.es/thredds/fileServer/mohid_{grid.grid_mohid}/files/%Y%m%d/'}
+        return date.strftime(URL[type_url])+self.get_file(type_url, name_grid, date)
+
+    def get_file(self, type_url,name_grid, date):
+
+        file = {'hydro_raw':
+           f'MOHID_Hydrodynamic_{self.GRID[name_grid].capital_grid}_%Y%m%d_0000.hdf5',
+           'hydro_hist':
+           f'MOHID_Hydrodynamic_{self.GRID[name_grid].capital_grid}_%Y%m%d_0000.hdf5',
+           'hydro_nc':
+           f'MOHID_{self.GRID[name_grid].capital_grid}_%Y%m%d_0000.nc4'}
+        return date.strftime(file[type_url])
+
 if __name__ == '__main__':
-    main()
+
+   main()
