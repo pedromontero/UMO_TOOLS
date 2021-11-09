031d6ada72d7dca5d61798070aa56a3e4e64bb95
Pedro Montero <pmontero@intecmar.gal>
2021-11-09 15:21:05 +0100

    read is broken up in read_head and read_uv

--- drawmap/src/drawmap.py
+++ drawmap/src/drawmap.py
@@ -61,7 +61,8 @@ def main():
     # Read input file
     inputs = read_inputs('drawmap.json')
     draw_map = DrawMap(inputs)
-    draw_map.read()
+    draw_map.read_head()
+    draw_map.reader_uv()
     draw_map.draw()
 
 
@@ -72,61 +73,73 @@ class DrawMap:
     """Class to draw a map with all options"""
 
     def __init__(self, inputs):
+
         self.file_path = inputs['path_in']
         self.file_in = inputs['file_in']
+        self.file_name = os.path.join(self.file_path, self.file_in)
+        self.file_hdf_out = inputs['file_out']
+        self.file_out = os.path.join(self.file_path, self.file_hdf_out)
+
         self.nx = inputs['nx']
         self.ny = inputs['ny']
         self.scale = inputs['scale']
         self.resolution = inputs['resolution']
+        self.style = inputs['style']
+        self.title = inputs['title']
+
         self.level = inputs['n_level']
         self.time = inputs['n_time']
-        self.file_hdf_out = inputs['file_out']
-        self.file_out = os.path.join(self.file_path, self.file_hdf_out)
-        self.title = inputs['title']
-        self.file_name = os.path.join(self.file_path, self.file_in)
-        self.style = inputs['style']
         limits = inputs['limits']
         self.boundary_box = BoundaryBox(limits[0], limits[1], limits[2], limits[3])
-
         self.u_name = inputs['u']
         self.v_name = inputs['v']
 
-    def read(self):
+        self.reader = None
+
+
+
+    def read_head(self):
 
         print('Opening: {0}'.format(self.file_name))
 
         factory = read_factory(self.file_name)
         self.reader = factory.get_reader()
 
-        data = self.reader.get_date(self.time)
-        data_str = data.strftime("%Y-%m-%d %H:%M UTC")
-        data_comp = data.strftime("%Y%m%d%H%M")
-        self.title = self.title + " " + data_str
-        self.file_out = self.file_out + '_' + data_comp + '.png'
+        with self.reader.open():
+            data = self.reader.get_date(self.time)
+            data_str = data.strftime("%Y-%m-%d %H:%M UTC")
+            data_comp = data.strftime("%Y%m%d%H%M")
+            self.title = self.title + " " + data_str
+            self.file_out = self.file_out + '_' + data_comp + '.png'
 
-        lat = self.reader.latitudes
-        lon = self.reader.longitudes
+            lat = self.reader.latitudes
+            lon = self.reader.longitudes
 
-        u = self.reader.get_variable(self.u_name, self.time)
-        v = self.reader.get_variable(self.v_name, self.time)
+            if self.reader.coordinates_rank == 1:
+                self.lats = lat[0:self.reader.n_latitudes - 1]
+                self.lons = lon[0:self.reader.n_longitudes - 1]
+            elif self.reader.coordinates_rank == 2:
+                self.lats = lat[0:self.reader.n_longitudes - 1, 0:self.reader.n_latitudes - 1]
+                self.lons = lon[0:self.reader.n_longitudes - 1, 0:self.reader.n_latitudes - 1]
 
-        if self.reader.coordinates_rank == 1:
-            self.lats = lat[0:self.reader.n_latitudes - 1]
-            self.lons = lon[0:self.reader.n_longitudes - 1]
-        elif self.reader.coordinates_rank == 2:
-            self.lats = lat[0:self.reader.n_longitudes - 1, 0:self.reader.n_latitudes - 1]
-            self.lons = lon[0:self.reader.n_longitudes - 1, 0:self.reader.n_latitudes - 1]
 
-        if len(u.shape) == 3:
-            self.us = u[self.level, 0:self.reader.n_latitudes - 1, 0:self.reader.n_longitudes - 1]
-            self.vs = v[self.level, 0:self.reader.n_latitudes - 1, 0:self.reader.n_longitudes - 1]
-        elif len(u.shape) == 2:
-            self.us = u[0:self.reader.n_latitudes - 1, 0:self.reader.n_longitudes - 1]
-            self.vs = v[0:self.reader.n_latitudes - 1, 0:self.reader.n_longitudes - 1]
+    def reader_uv(self):
 
-        self.mod = pow((pow(self.us, 2) + pow(self.vs, 2)), .5)
+        with self.reader.open():
+            u = self.reader.get_variable(self.u_name, self.time)
+            v = self.reader.get_variable(self.v_name, self.time)
 
-        self.reader.close()
+
+
+            if len(u.shape) == 3:
+                self.us = u[self.level, 0:self.reader.n_latitudes - 1, 0:self.reader.n_longitudes - 1]
+                self.vs = v[self.level, 0:self.reader.n_latitudes - 1, 0:self.reader.n_longitudes - 1]
+            elif len(u.shape) == 2:
+                self.us = u[0:self.reader.n_latitudes - 1, 0:self.reader.n_longitudes - 1]
+                self.vs = v[0:self.reader.n_latitudes - 1, 0:self.reader.n_longitudes - 1]
+
+            self.mod = pow((pow(self.us, 2) + pow(self.vs, 2)), .5)
+
 
     def draw(self):
 
