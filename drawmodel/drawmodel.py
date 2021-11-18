db169962c61a1954c86dcf18926e2091312f089e
Pedro Montero <pmontero@intecmar.gal>
2021-11-18 07:55:49 +0100

    drawmodel is able to download several days

--- drawmodel/drawmodel.py
+++ drawmodel/drawmodel.py
@@ -1,9 +1,18 @@
 from download_models import download_hydro
+from common import read_input
 
 
-def draw_model():
-    pass
+def draw_model(inputs):
+    dates = inputs['dates']
+    for date in dates:
+        inputs['date_ini'] = date
+        download_hydro.main(inputs)
+
 
 
 if __name__ == '__main__':
-    draw_model()
+    # input
+    input_keys = ['type_url', 'name_grid', 'dates', 'days', 'path_out']
+    inputs = read_input('download_models.json', input_keys)
+    # end input
+    draw_model(inputs)
