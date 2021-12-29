d86b292f07cc680988e615778cfaebec73ae2144
Pedro Montero <pmontero@intecmar.gal>
2021-12-29 14:01:49 +0100

    tuv2nc.py and rest moved to main folder

--- codar2nc/tuv2thredds.py
+++ codar2nc/tuv2thredds.py
@@ -3,7 +3,7 @@ import re
 import shutil
 from datetime import datetime, timedelta
 from create_dir_structure import FolderTree
-from tuv2nc import tuv2nc
+from codar2nc.tuv2nc import tuv2nc
 import getradarfiles
 
 
@@ -18,31 +18,32 @@ def check_nc_file(full_file):
 
 class Tuv2Nc:
     def __init__(self, data_folder, system):
-        self.system = system
-        self.tuv_folder = os.path.join(data_folder, 'radarhf_tmp', 'tuv')
+
+        self.tuv_folder = os.path.join(data_folder, 'radarhf_tmp', 'tuv', system)
         self.root_folder = os.path.join(data_folder, 'radarhf/dev/RadarOnRAIA/Totals/v2.2')
         self.nc_folder = os.path.join(data_folder, 'radarhf_tmp', 'nc', 'total')
 
     def check_nc_files(self, thredds):
 
         for file in os.listdir(self.tuv_folder):
+            print(f'file = {file}')
             site = re.findall("[A-Z]{4}", file.split('/')[-1])[0]
             day = datetime.strptime('%s%s%s%s' % tuple(re.findall("\d+", file.split('/')[-1])), '%Y%m%d%H%M')
 
-            thredds.make_total_folder( day)
-            full_file = os.path.join(self.root_folder, thredds.get_full_total_file_nc( day))
+            thredds.make_total_folder(day)
+            full_file = os.path.join(self.root_folder, thredds.get_full_total_file_nc(day))
             if not check_nc_file(full_file):
-                print(f'---------------- vou crear {full_file}')
+                print(f'Vou crear {full_file}')
 
                 day_hour_before = day + timedelta(hours=-1)
-                file_previous_hour = os.path.join(self.root_folder, thredds.get_full_total_file_nc( day_hour_before))
+                file_previous_hour = os.path.join(self.root_folder, thredds.get_full_total_file_nc(day_hour_before))
                 if check_nc_file(file_previous_hour):
                     shutil.copy(file_previous_hour, self.nc_folder)
                 day_2hour_previous = day + timedelta(hours=-2)
                 file_2hour_previous = os.path.join(self.root_folder, thredds.get_full_total_file_nc(day_2hour_previous))
                 if check_nc_file(file_2hour_previous):
                     shutil.copy(file_2hour_previous, self.nc_folder)
-                print(f'---vou necesitar {file_previous_hour} e {file_2hour_previous}')
+                print(f'Necesito {file_previous_hour} , {file_2hour_previous}')
                 try:
                     print(self.tuv_folder, self.nc_folder, file)
                     tuv2nc(self.tuv_folder, self.nc_folder, file)
@@ -51,7 +52,7 @@ class Tuv2Nc:
                 except KeyError as e:
                     print("Error: KeyError", e)
                 except:
-                    print("Another exception")
+                    print("----------------------------------------------------------------> Another exception!!!!")
                 else:
                    shutil.move(os.path.join(self.nc_folder, thredds.get_name('Total', day)), full_file)
                 shutil.rmtree(self.nc_folder)
@@ -59,7 +60,7 @@ class Tuv2Nc:
 
 
 def tuv2thredds(data_folder):
-    system = 'Galicia'
+    system = 'GALI'
 
     tuv2nc = Tuv2Nc(data_folder, system)
     thredds = FolderTree(tuv2nc.root_folder)
@@ -67,7 +68,6 @@ def tuv2thredds(data_folder):
 
 
 if __name__ == '__main__':
-    data_folder = r'../../datos/'
-    # getradarfiles.get_radar_files(data_folder)
-
+    data_folder = r'../datos/'
+    getradarfiles.get_total_files(data_folder)
     tuv2thredds(data_folder)
