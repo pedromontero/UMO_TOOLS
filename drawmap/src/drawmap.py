630347fa11769b88cee1d4fa4ccbd7af7ba8caf7
Pedro <pmontero@intecmar.gal>
2022-07-28 08:48:28 +0200

    several changes: scalar drawmap added options is a new object, there are another two classes: Vector and Scalar, sons of DrawMap

--- drawmap/src/drawmap.py
+++ drawmap/src/drawmap.py
@@ -41,7 +41,9 @@ def read_inputs(input_file):
                   'n_level',
                   'title',
                   'style',
-                  'limits']
+                  'limits',
+                  'vector',
+                  'scalar']
     return read_input(input_file, input_keys)
 
 
@@ -61,20 +63,28 @@ def main():
 
     # Read input file
     inputs = read_inputs('drawmap.json')
-    draw_map_1(inputs, 3)
+    if inputs['vector']:
+        draw_map_vector(inputs, 3)
+    if inputs['scalar']:
+        draw_map_scalar(inputs, 3)
     #draw_map_24(inputs)
 
-def draw_map_1(inputs, n ):
+def draw_map_vector(inputs, n ):
     """draw 1 maps of a day"""
-    draw_map = DrawMap(inputs)
+    draw_map = Vector(inputs)
     draw_map.read_head()
-
     draw_map.create_title(n)
-    draw_map.reader_uv_by_time(n)
-    print(draw_map.lats.shape, draw_map.lons.shape)
-    print(draw_map.us.shape, draw_map.vs.shape)
-    print(draw_map.title_full)
+
+    draw_map.reader_by_time()
     draw_map.draw()
+
+def draw_map_scalar(inputs, n):
+    draw_map = Scalar(inputs)
+    draw_map.read_head()
+    draw_map.create_title(n)
+
+    draw_map.reader_by_time()
+    draw_map.draw_scalar()
 
 def draw_map_24(inputs):
     """draw 24+1 maps of a day"""
@@ -83,15 +93,47 @@ def draw_map_24(inputs):
 
     for n in range(draw_map.reader.ini_ntime, 25+draw_map.reader.ini_ntime):
         draw_map.create_title(n)
-        draw_map.reader_uv_by_time(n)
-        print(draw_map.title_full)
 
-        draw_map.draw()
+        if draw_map.vector_bool:
+            draw_map.vector.reader_by_time(draw_map)
+            draw_map.vector.draw(draw_map)
 
 
 class DrawMap:
 
-    """Class to draw a map with all options"""
+    def __init__(self, inputs):
+        self.options = OptionsMap(inputs)
+        self.reader = self.get_reader(self.options.file_name)
+
+    def read_head(self):
+
+        with self.reader.open():
+
+            lat = self.reader.latitudes
+            lon = self.reader.longitudes
+
+            if self.reader.coordinates_rank == 1:
+                self.lats = lat[0:self.reader.n_latitudes - 2]  # ATENCIóN:Esto y lo de abajo era -1, revisar
+                self.lons = lon[0:self.reader.n_longitudes - 2]
+            elif self.reader.coordinates_rank == 2:
+                self.lats = lat[0:self.reader.n_longitudes - 2, 0:self.reader.n_latitudes - 2]
+                self.lons = lon[0:self.reader.n_longitudes - 2, 0:self.reader.n_latitudes - 2]
+
+    def create_title(self, n_time):
+        with self.reader.open():
+            data = self.reader.get_date(n_time)
+            data_str = data.strftime("%Y-%m-%d %H:%M UTC")
+            data_comp = data.strftime("%Y%m%d%H%M")
+            self.title_full = self.options.title + " " + data_str
+            self.file_out_full = self.options.file_out + '_' + data_comp + '.png'
+
+    def get_reader(self, file_in):
+        print('Opening: {0}'.format(file_in))
+        factory = read_factory(file_in)
+        return factory.get_reader()
+
+
+class OptionsMap:
 
     def __init__(self, inputs):
 
@@ -113,60 +155,124 @@ class DrawMap:
         self.time = inputs['n_time']
         limits = inputs['limits']
         self.boundary_box = BoundaryBox(limits[0], limits[1], limits[2], limits[3])
+
+        self.vector_bool = inputs['vector']
+        self.scalar_bool = inputs['scalar']
+
+
+class Vector(DrawMap):
+
+    def __init__(self, inputs):
+
+        super().__init__(inputs)
+
+
         self.u_name = inputs['u']
         self.v_name = inputs['v']
+        self.us = None
+        self.vs = None
+        self.modules = None
 
-        self.reader = None
 
-    def read_head(self):
-        print('Opening: {0}'.format(self.file_name))
-
-        factory = read_factory(self.file_name)
-        self.reader = factory.get_reader()
+    def reader_by_time(self):
 
         with self.reader.open():
-
-            lat = self.reader.latitudes
-            lon = self.reader.longitudes
-
-            if self.reader.coordinates_rank == 1:
-                self.lats = lat[0:self.reader.n_latitudes - 2]  # ATENCIóN:Esto y lo de abajo era -1, revisar
-                self.lons = lon[0:self.reader.n_longitudes - 2]
-            elif self.reader.coordinates_rank == 2:
-                self.lats = lat[0:self.reader.n_longitudes - 2, 0:self.reader.n_latitudes - 2]
-                self.lons = lon[0:self.reader.n_longitudes - 2, 0:self.reader.n_latitudes - 2]
-
-    def create_title(self, n_time):
-        with self.reader.open():
-            data = self.reader.get_date(n_time)
-            data_str = data.strftime("%Y-%m-%d %H:%M UTC")
-            data_comp = data.strftime("%Y%m%d%H%M")
-            self.title_full = self.title + " " + data_str
-            self.file_out_full = self.file_out + '_' + data_comp + '.png'
-
-    def reader_uv_by_time(self, n_time):
-
-        with self.reader.open():
-            u = self.reader.get_variable(self.u_name, n_time)
-            v = self.reader.get_variable(self.v_name, n_time)
+            u = self.reader.get_variable(self.u_name, self.options.time)
+            v = self.reader.get_variable(self.v_name, self.options.time)
 
             if len(u.shape) == 3:
-                self.us = u[self.level, :-1, :- 1]
-                self.vs = v[self.level, :-1, :-1]
+                self.us = u[self.options.level, :-1, :- 1]
+                self.vs = v[self.options.level, :-1, :-1]
 
             elif len(u.shape) == 2:
                 self.us = u[:-1, :-1]
                 self.vs = v[:-1, :-1]
 
-            self.mod = pow((pow(self.us, 2) + pow(self.vs, 2)), .5)
-
-    def reader_uv(self):
-        self.reader_uv_by_time(self.time)
+            self.modules = pow((pow(self.us, 2) + pow(self.vs, 2)), .5)
 
     def draw(self):
-        drawcurrents(self.reader.coordinates_rank, self.nx, self.ny, self.scale, self.resolution,
-                     self.level, self.time, self.lats, self.lons, self.us, self.vs, self.mod,
-                     self.file_out_full, self.title_full, self.style, self.boundary_box)
+        drawcurrents(self.reader.coordinates_rank, self.options.nx, self.options.ny,
+                     self.options.scale, self.options.resolution,
+                     self.options.level, self.options.time,
+                     self.lats, self.lons, self.us, self.vs, self.modules,
+                     self.file_out_full, self.title_full, self.options.style,
+                     self.options.boundary_box)
+
+
+class Scalar(DrawMap):
+
+    def __init(self, inputs):
+
+        super().__init__(inputs)
+        self.scalar_name = inputs['scalar_magnitude']
+        self.scalars = None
+
+    def reader_by_time(self):
+
+        with self.reader.open():
+            scalar = self.reader.get_variable(self.scalar_name, self.options.time)
+
+            if len(scalar.shape) == 3:
+                self.scalars = scalar[self.options.level, :-1, :- 1]
+
+            elif len(scalar.shape) == 2:
+                self.scalars = scalar[:-1, :-1]
+
+    def draw_scalar(self):
+        """
+
+        :return:
+        """
+        import numpy as np
+        import matplotlib.pyplot as plt
+        from mpl_toolkits.basemap import Basemap
+
+        fig = plt.figure(figsize=(10, 10))
+        ax = fig.add_subplot(111)
+
+        dx = 0.01
+        middle_lon = self.options.boundary_box.middle_lon()
+        middle_lat =  self.options.boundary_box.middle_lat()
+        m = Basemap(llcrnrlon=self.options.boundary_box.lon_min - dx,
+                    llcrnrlat=self.options.boundary_box.lat_min - dx,
+                    urcrnrlon= self.options.boundary_box.lon_max + dx,
+                    urcrnrlat=self.options.boundary_box.lat_max + dx,
+                    resolution=self.options.resolution, projection='tmerc', lon_0=middle_lon, lat_0=middle_lat)
+
+        m.drawcoastlines()
+        m.fillcontinents(color='grey', lake_color='aqua')
+
+        # m.drawmapboundary(fill_color='aqua')
+
+        if self.reader.coordinates_rank == 1:
+            lon, lat = np.meshgrid(self.lons, self.lats)
+        else:
+            lon, lat = self.lons, self.lats
+
+        x, y = m(lon, lat)
+
+        # draw filled contours.
+        clevs = [0, 0.1, 0.2, 0.3, 0.4, 0.5]
+        # clevs = [0,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1]
+
+        cs = m.contourf(x, y, self.scalars, clevs, cmap=plt.cm.jet)
+        cbar = m.colorbar(cs, location='bottom', pad="5%")
+        cbar.ax.tick_params(labelsize=9)
+        cbar.set_label('m/s')
+
+        # add title
+        plt.title(self.title_full)
+
+        fig.savefig(self.file_out_full, dpi=100, facecolor='w', edgecolor='w', format='png',
+                    transparent=False, bbox_inches='tight', pad_inches=0.1)
+        plt.clf()
+        plt.close(fig)
+
+        return
+
+
+
+
         
 if __name__ == '__main__':
     main()
